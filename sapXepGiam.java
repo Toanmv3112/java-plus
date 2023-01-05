12d644c42d7f1c1c311ff4f3905b5d2728c25c74
unknown <toan.hust@gmail.com>
2023-01-06 01:39:26 +0700

    sapXepGiam

---
+++ sapXepGiam.java
@@ -0,0 +1,18 @@
+public class sapXepGiam {
+    public static void main(String[] args) {
+        int arr[] = {3, 9, 10, 4, 1, 6, 4, 2, 4, 5, 6};
+        int trungGian;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    trungGian=arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j+1]= trungGian;
+                }
+            }
+        }
+        for (int a = 0; a < arr.length; a++) {
+            System.out.println(arr[a]);
+        }
+    }
+}
